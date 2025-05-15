b2307219900696a41df5f2086c198ad6c7ff42d1
notHyotic <hy0t1c1@gmail.com>
2025-05-15 04:52:39 -0500

    fix ssm

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -1,13 +1,19 @@
 resource "aws_ssm_parameter" "new_relic_license_key" {
   name        = "/runner-metrics-lambda/newrelic/license_key"
   type        = "SecureString"
-  value       = var.new_relic_license_key
+  value       = "unintialized"
   description = "New Relic License Key for runner-metrics-lambda"
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
 
 resource "aws_ssm_parameter" "new_relic_user_key" {
   name        = "/runner-metrics-lambda/newrelic/user_key"
   type        = "SecureString"
-  value       = var.new_relic_user_key
+  value       = "unintialized"
   description = "New Relic User Key for runner-metrics-lambda"
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
