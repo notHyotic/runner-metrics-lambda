482ab12c10849ab42196071cca74f49d68b24b3a
notHyotic <hy0t1c1@gmail.com>
2025-05-15 01:16:16 -0500

    add tfplan and tfapply

---
+++ .ops/commands/tfplan.go
@@ -0,0 +1,30 @@
+package commands
+
+import (
+	"log"
+
+	"lesiw.io/cmdio/sys"
+)
+
+func (Ops) Tfplan() {
+	var rnr = sys.Runner().WithEnv(map[string]string{
+		"PWD": "./terraform",
+	})
+
+	defer rnr.Close()
+
+	err := rnr.Run("terraform", "init")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = rnr.Run("terraform", "validate")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = rnr.Run("terraform", "plan")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
